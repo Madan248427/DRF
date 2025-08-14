0aa700296712c5b1488f1efd8dc6635628728782
Madan248427 <katuwalmadan55@gmail.com>
2025-08-15 00:33:02 +0545

    Update settings.py

--- datingapp/settings.py
+++ datingapp/settings.py
@@ -93,11 +93,14 @@ WSGI_APPLICATION = "datingapp.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # make sure BASE_DIR is defined, typically in settings.py
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Mrk@0404',
+        'HOST': 'db.uwzjrgdotdwoankiynhz.supabase.co',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
