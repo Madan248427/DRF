22997dcaec8e367d857c16024bf42181dca19b41
Madan248427 <katuwalmadan55@gmail.com>
2025-08-16 06:38:27 +0545

    Update settings.py

--- datingapp/settings.py
+++ datingapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--1a#396c(=c6b4gq^z+d74!*s%lu_tle!qe@d_qpu+zqrj@9o("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
